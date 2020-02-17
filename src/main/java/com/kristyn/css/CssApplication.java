40afb30237a712f0b14c59c70529eab1facbbf59
GBTC440014UR <GBTC440014UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-17 09:24:04 -0500

    initial commit

---
+++ src/main/java/com/kristyn/css/CssApplication.java
@@ -0,0 +1,13 @@
+package com.kristyn.css;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CssApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kristyn/css/CssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kristyn.css;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
